e3954eaebb0a797df4eff69b3c78684e06f7411f
Salma-ssss <445052168@std.psau.edu.sa>
2026-05-03 01:13:41 +0300

    Use Semaphore to control CPU access in process execution and in the final block we release the SharedResources Semaphore

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -125,7 +125,12 @@ class Process implements Runnable {
     public void run() {
         // TODO #3: Acquire CPU semaphore before executing
         // This ensures only allowed number of processes run simultaneously
-
+        // Acquire CPU access before execution
+        try {
+            SharedResources.cpuSemaphore.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         try {
             if (startTime == -1) {
                 startTime = System.currentTimeMillis();
@@ -188,6 +193,7 @@ class Process implements Runnable {
         } finally {
             // TODO #4: Release CPU semaphore here
             // Always release in finally block to prevent deadlocks!
+            SharedResources.cpuSemaphore.release();
         }
     }
 
